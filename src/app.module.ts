e6edd50a6e98c816e39b1ffc1b97c5a2412e19f6
Bartosz Wojciechowski <bartosz347@gmail.com>
2021-09-20 19:22:17 +0200

    Load DB config from env variables, run migrations automatically on each app launch

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import * as ormconfig from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
